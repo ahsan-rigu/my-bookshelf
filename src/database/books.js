4af246d1cf53c6114e0eba459035198768249dec
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-06-17 19:32:11 +0530

    feat- CRA, feat- database (without images)

---
+++ src/database/books.js
@@ -0,0 +1,112 @@
+const books = [
+  {
+    name: "Pride and Prejudice",
+    image: "https://example.com/pride_and_prejudice.jpg",
+    author: "Jane Austen",
+    status: "read",
+  },
+  {
+    name: "To Kill a Mockingbird",
+    image: "https://example.com/to_kill_a_mockingbird.jpg",
+    author: "Harper Lee",
+    status: "reading",
+  },
+  {
+    name: "1984",
+    image: "https://example.com/1984.jpg",
+    author: "George Orwell",
+    status: "to be read",
+  },
+  {
+    name: "Moby-Dick",
+    image: "https://example.com/moby_dick.jpg",
+    author: "Herman Melville",
+    status: "read",
+  },
+  {
+    name: "The Great Gatsby",
+    image: "https://example.com/the_great_gatsby.jpg",
+    author: "F. Scott Fitzgerald",
+    status: "reading",
+  },
+  {
+    name: "War and Peace",
+    image: "https://example.com/war_and_peace.jpg",
+    author: "Leo Tolstoy",
+    status: "to be read",
+  },
+  {
+    name: "Jane Eyre",
+    image: "https://example.com/jane_eyre.jpg",
+    author: "Charlotte Brontë",
+    status: "reading",
+  },
+  {
+    name: "The Catcher in the Rye",
+    image: "https://example.com/the_catcher_in_the_rye.jpg",
+    author: "J.D. Salinger",
+    status: "to be read",
+  },
+  {
+    name: "Frankenstein",
+    image: "https://example.com/frankenstein.jpg",
+    author: "Mary Shelley",
+    status: "read",
+  },
+  {
+    name: "The Adventures of Huckleberry Finn",
+    image: "https://example.com/huckleberry_finn.jpg",
+    author: "Mark Twain",
+    status: "reading",
+  },
+  {
+    name: "Don Quixote",
+    image: "https://example.com/don_quixote.jpg",
+    author: "Miguel de Cervantes",
+    status: "to be read",
+  },
+  {
+    name: "Pride and Prejudice",
+    image: "https://example.com/pride_and_prejudice.jpg",
+    author: "Jane Austen",
+    status: "read",
+  },
+  {
+    name: "To Kill a Mockingbird",
+    image: "https://example.com/to_kill_a_mockingbird.jpg",
+    author: "Harper Lee",
+    status: "reading",
+  },
+  {
+    name: "The Picture of Dorian Gray",
+    image: "https://example.com/dorian_gray.jpg",
+    author: "Oscar Wilde",
+    status: "to be read",
+  },
+  {
+    name: "Alice's Adventures in Wonderland",
+    image: "https://example.com/alice_in_wonderland.jpg",
+    author: "Lewis Carroll",
+    status: "reading",
+  },
+  {
+    name: "The Scarlet Letter",
+    image: "https://example.com/scarlet_letter.jpg",
+    author: "Nathaniel Hawthorne",
+    status: "read",
+  },
+  {
+    name: "The Count of Monte Cristo",
+    image: "https://example.com/count_of_monte_cristo.jpg",
+    author: "Alexandre Dumas",
+    status: "to be read",
+  },
+  {
+    name: "Wuthering Heights",
+    image: "https://example.com/wuthering_heights.jpg",
+    author: "Emily Brontë",
+    status: "reading",
+  },
+];
+
+export default books;
