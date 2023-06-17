ef6297b48e835fd14d809a6f15fee191efc8faaa
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-06-17 22:35:27 +0530

    fix- image links

--- src/database/books.js
+++ src/database/books.js
@@ -11,7 +11,7 @@ const books = [
     id: 2,
     name: "To Kill a Mockingbird",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://cdn.britannica.com/21/182021-050-666DB6B1/book-cover-To-Kill-a-Mockingbird-many-1961.jpg",
     author: "Harper Lee",
     status: "reading",
   },
@@ -19,15 +19,14 @@ const books = [
     id: 3,
     name: "1984",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/519zR2oIlmL._AC_UF1000,1000_QL80_.jpg",
     author: "George Orwell",
     status: "to be read",
   },
   {
     id: 4,
     name: "Moby-Dick",
-    image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+    image: "https://m.media-amazon.com/images/I/51NngkgAIpL.jpg",
     author: "Herman Melville",
     status: "read",
   },
@@ -35,7 +34,7 @@ const books = [
     id: 5,
     name: "The Great Gatsby",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/61z0MrB6qOS._AC_UF1000,1000_QL80_.jpg",
     author: "F. Scott Fitzgerald",
     status: "reading",
   },
@@ -43,15 +42,14 @@ const books = [
     id: 6,
     name: "War and Peace",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/51s4UBf-y8L._AC_UF1000,1000_QL80_.jpg",
     author: "Leo Tolstoy",
     status: "to be read",
   },
   {
     id: 7,
     name: "Jane Eyre",
-    image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+    image: "https://m.media-amazon.com/images/I/31BW+LcZZjL.jpg",
     author: "Charlotte Brontë",
     status: "reading",
   },
@@ -59,7 +57,7 @@ const books = [
     id: 8,
     name: "The Catcher in the Rye",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/71fWBj3qq+L._AC_UF1000,1000_QL80_.jpg",
     author: "J.D. Salinger",
     status: "to be read",
   },
@@ -67,7 +65,7 @@ const books = [
     id: 9,
     name: "Frankenstein",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/71hCBEMpQ0L._AC_UF1000,1000_QL80_.jpg",
     author: "Mary Shelley",
     status: "read",
   },
@@ -75,13 +73,13 @@ const books = [
     id: 10,
     name: "Don Quixote",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://kbimages1-a.akamaihd.net/4751e5d4-13de-4372-955c-ccde274a6d20/1200/1200/False/don-quixote-47.jpg",
     author: "Miguel de Cervantes",
     status: "to be read",
   },
   {
     id: 11,
-    name: "To Kill a Mockingbird",
+    name: "To Kill a Mockingbird 2",
     image:
       "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
     author: "Harper Lee",
@@ -90,8 +88,7 @@ const books = [
   {
     id: 12,
     name: "The Picture of Dorian Gray",
-    image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+    image: "https://mpd-biblio-covers.imgix.net/9781466805064.jpg",
     author: "Oscar Wilde",
     status: "to be read",
   },
@@ -99,15 +96,14 @@ const books = [
     id: 13,
     name: "The Scarlet Letter",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://m.media-amazon.com/images/I/61pLRWYTbXL._AC_UF1000,1000_QL80_.jpg",
     author: "Nathaniel Hawthorne",
     status: "read",
   },
   {
     id: 14,
     name: "The Count of Monte Cristo",
-    image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+    image: "https://m.media-amazon.com/images/I/51W5xvphHlL.jpg",
     author: "Alexandre Dumas",
     status: "to be read",
   },
@@ -115,7 +111,7 @@ const books = [
     id: 15,
     name: "Wuthering Heights",
     image:
-      "https://cdn.myportfolio.com/15658835-56a5-4669-9d59-ac3bfc57684b/4ce45f83-54b0-4b2a-a982-26121d9a4df5_rw_1920.png?h=264575f403947d7069d4007280363b6c",
+      "https://images.squarespace-cdn.com/content/v1/5d40204073334a0001f2f066/1602439836717-GUAAN9EXNKDOGD9SKKZM/3674767a84174c9df69d8706e755e37a.jpg",
     author: "Emily Brontë",
     status: "reading",
   },
